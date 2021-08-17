1795c0ac74737c395a4f2128a7055e3feeedd685
Manish Kumar <manishegley@gmail.com>
2021-08-17 18:50:51 +0530

    File updated

--- voucherhub/manage.py
+++ voucherhub/manage.py
@@ -16,7 +16,7 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-# Edited by Manish Kumar Singh
+## Edited by Manish Kumar Singh
 
 if __name__ == '__main__':
     main()
